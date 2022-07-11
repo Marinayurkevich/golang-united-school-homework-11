a53fe348d58514ccbe7e3c73a1c69dc782b0f91c
marina <roxy-lo@inbox.ru>
2022-07-11 19:13:56 +0300

    result

--- lib/batch/batch.go
+++ lib/batch/batch.go
@@ -9,7 +9,7 @@ type user struct {
 	ID int64
 }
 
-func getOne(id int64, wg *sync.WaitGroup) user {
+func getOne(id int64) user {
 	time.Sleep(time.Millisecond * 100)
 	return user{ID: id}
 }
@@ -23,7 +23,7 @@ func getBatch(n int64, pool int64) (res []user) {
 		wg.Add(1)
 		ch <- 0
 		go func(j int64) {
-			OneUser := getOne(j, &wg)
+			OneUser := getOne(j)
 			mx.Lock()
 			res = append(res, OneUser)
 			mx.Unlock()
